ff8c2ea2744a40a8f66cdf7223b09709cd28d14e
lzhar <henrique.luiz.alves@gmail.com>
2026-04-09 12:01:34 -0300

    Teste commit

--- src/main/java/com/luiz/fsociety/FsocietyApplication.java
+++ src/main/java/com/luiz/fsociety/FsocietyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FsocietyApplication {
+public class 														FsocietyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FsocietyApplication.class, args);
